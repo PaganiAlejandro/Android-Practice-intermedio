89b77e5a25ea0cfd82b78e3a6d03a7fb6acfd99b
AlejandroPaganiTricks <alejandropagani@7r1ck.com>
2024-03-28 12:34:44 -0300

    initial commit

---
+++ app/src/main/java/com/alepagani/horoscapp/ui/home/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.alepagani.horoscapp.ui.home
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.alepagani.horoscapp.R
+import com.alepagani.horoscapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initUI()
+    }
+
+    private fun initUI() {
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerNav) as NavHostFragment
+        navController = navHost.navController
+        binding.bottomNavHost.setupWithNavController(navController)
+    }
+}
